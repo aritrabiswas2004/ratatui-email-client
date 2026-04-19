afdbfb9ed91f20e92cb21c57f5a5c6163d1c0634
Aritra Biswas <aritrabb@gmail.com>
2026-04-19 12:24:12 +0200

    Make GH ready init project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Ratatui Email Client
+
+Open source email client written in Rust.
+
+## Build Instructions
+
+Clone this repo and run
+
+```shell
+cargo build
+```
+
+or run directly with
+
+```shell
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> Result<(), io::Error>{
 
     let mut counter = 0;
 
-    terminal.clear();
+    terminal.clear().expect("Failed to CLSSCR");
 
     loop {
         terminal.draw(|f| {
@@ -29,26 +29,17 @@ fn main() -> Result<(), io::Error>{
             let chunks = Layout::default()
                 .direction(Direction::Vertical)
                 .constraints([
-                    Constraint::Percentage(50),
-                    Constraint::Percentage(50)
+                    Constraint::Percentage(100),
                 ])
                 .split(size);
 
             let block = Block::default()
-                .title("Testing the TUI")
+                .title("Testing the TUI - Press q to quit")
                 .borders(Borders::ALL);
-
-            let desc_block = Block::default()
-                .title("Second Block")
-                .borders(Borders::ALL);
-
 
             let paragraph = Paragraph::new(format!("Counter: {}", counter)).block(block);
 
-            let desc_para = Paragraph::new("Description").block(desc_block);
-
             f.render_widget(paragraph, chunks[0]);
-            f.render_widget(desc_para, chunks[1]);
         })?;
 
         // Handle input
